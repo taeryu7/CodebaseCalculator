24aa5d0cf9558d1feea8b1fb76ba654691fe6b66
taeryu7 <taeryu97@gmail.com>
2024-11-14 20:12:26 +0900

    LV3. 나머지 버튼들 추가

--- CodebaseCalculator/CodebaseCalculator/ViewController.swift
+++ CodebaseCalculator/CodebaseCalculator/ViewController.swift
@@ -22,6 +22,25 @@ class ViewController: UIViewController {
     let eightButton = UIButton()
     let nineButton = UIButton()
     let stackView = UIStackView()
+    // 두번째 줄
+    let fourButton = UIButton()
+    let fiveButton = UIButton()
+    let sixButton = UIButton()
+    let minusButton = UIButton()
+    let stackView1 = UIStackView()
+    // 세번째 줄
+    let oneButton = UIButton()
+    let twoButton = UIButton()
+    let threeButton = UIButton()
+    let multplyButton = UIButton()
+    let stackView2 = UIStackView()
+    
+    // 네번째 줄
+    let resetButton = UIButton()
+    let zeroButton = UIButton()
+    let equalButton = UIButton()
+    let dividButton = UIButton()
+    let stackView3 = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +62,24 @@ class ViewController: UIViewController {
         setupButton(button: sevenButton, title: "7")
         setupButton(button: eightButton, title: "8")
         setupButton(button: nineButton, title: "9")
+        
+        // 두번째 버튼 설정
+        setupButton(button: fourButton, title: "4")
+        setupButton(button: fiveButton, title: "5")
+        setupButton(button: sixButton, title: "6")
+        setupButton(button: minusButton, title: "-")
+        
+        //세번째 버튼 설정
+        setupButton(button: oneButton, title: "1")
+        setupButton(button: twoButton, title: "2")
+        setupButton(button: threeButton, title: "3")
+        setupButton(button: multplyButton, title: "*")
+        
+        //네번째 버튼 설정
+        setupButton(button: resetButton, title: "AC")
+        setupButton(button: zeroButton, title: "0")
+        setupButton(button: equalButton, title: "=")
+        setupButton(button: dividButton, title: "/")
 
         // 1.스택뷰 설정
         stackView.axis = .horizontal
@@ -53,9 +90,48 @@ class ViewController: UIViewController {
         stackView.addArrangedSubview(nineButton)
         stackView.addArrangedSubview(plusButton)
         
+        // 1.스택뷰 설정
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.addArrangedSubview(sevenButton)
+        stackView.addArrangedSubview(eightButton)
+        stackView.addArrangedSubview(nineButton)
+        stackView.addArrangedSubview(plusButton)
+       
+        // 2.스택뷰 설정
+        stackView1.axis = .horizontal
+        stackView1.spacing = 10
+        stackView1.distribution = .fillEqually
+        stackView1.addArrangedSubview(fourButton)
+        stackView1.addArrangedSubview(fiveButton)
+        stackView1.addArrangedSubview(sixButton)
+        stackView1.addArrangedSubview(minusButton)
+       
+        // 3.스택뷰 설정
+        stackView2.axis = .horizontal
+        stackView2.spacing = 10
+        stackView2.distribution = .fillEqually
+        stackView2.addArrangedSubview(oneButton)
+        stackView2.addArrangedSubview(twoButton)
+        stackView2.addArrangedSubview(threeButton)
+        stackView2.addArrangedSubview(multplyButton)
+       
+        //4. 스택뷰 설정
+        stackView3.axis = .horizontal
+        stackView3.spacing = 10
+        stackView3.distribution = .fillEqually
+        stackView3.addArrangedSubview(resetButton)
+        stackView3.addArrangedSubview(zeroButton)
+        stackView3.addArrangedSubview(equalButton)
+        stackView3.addArrangedSubview(dividButton)
+        
         // 뷰 추가 필수로 해야함.
         view.addSubview(label)
         view.addSubview(stackView)
+        view.addSubview(stackView1)
+        view.addSubview(stackView2)
+        view.addSubview(stackView3)
         
         // 제약 조건 설정
         label.snp.makeConstraints {
@@ -66,13 +142,32 @@ class ViewController: UIViewController {
         }
         
         stackView.snp.makeConstraints {
-            $0.top.equalTo(label.snp.bottom).offset(60)
-            $0.leading.equalToSuperview().offset(30)
-            $0.trailing.equalToSuperview().offset(-30)
-            $0.height.equalTo(80)
-        }
+             $0.top.equalTo(label.snp.bottom).offset(60)
+             $0.leading.equalToSuperview().offset(30)
+             $0.trailing.equalToSuperview().offset(-30)
+             $0.height.equalTo(80)
+         }
         
+         stackView1.snp.makeConstraints {
+             $0.top.equalTo(stackView.snp.bottom).offset(10)
+             $0.leading.equalToSuperview().offset(30)
+             $0.trailing.equalToSuperview().offset(-30)
+             $0.height.equalTo(80)
+         }
         
+         stackView2.snp.makeConstraints {
+             $0.top.equalTo(stackView1.snp.bottom).offset(10)
+             $0.leading.equalToSuperview().offset(30)
+             $0.trailing.equalToSuperview().offset(-30)
+             $0.height.equalTo(80)
+         }
+        
+         stackView3.snp.makeConstraints {
+             $0.top.equalTo(stackView2.snp.bottom).offset(10)
+             $0.leading.equalToSuperview().offset(30)
+             $0.trailing.equalToSuperview().offset(-30)
+             $0.height.equalTo(80)
+         }
         
     }
     //숫자 버튼 설정
@@ -84,7 +179,7 @@ class ViewController: UIViewController {
     }
 
 }
+
 #Preview {
   ViewController()
 }
-
