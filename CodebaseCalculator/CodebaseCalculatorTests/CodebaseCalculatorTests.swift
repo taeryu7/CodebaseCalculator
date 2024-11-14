39ef6df5d655382b3d4c30628e574bfd3e33884f
taeryu7 <taeryu97@gmail.com>
2024-11-14 14:32:23 +0900

    test

---
+++ CodebaseCalculator/CodebaseCalculatorTests/CodebaseCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodebaseCalculatorTests.swift
+//  CodebaseCalculatorTests
+//
+//  Created by 유태호 on 11/14/24.
+//
+
+import Testing
+@testable import CodebaseCalculator
+
+struct CodebaseCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
